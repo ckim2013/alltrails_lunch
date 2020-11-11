e242c83851f6fad7fc1dda818bee32d024af9945
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2020-11-11 14:19:07 -0800

    Change class names in html and css

--- app/javascript/frontend/components/NavBar/NavBarView.jsx
+++ app/javascript/frontend/components/NavBar/NavBarView.jsx
@@ -7,12 +7,12 @@ const NavBarView = (props) => {
   } = props;
   
   return (
-    <div className="navbar-container">
-      <div className="title-container">
+    <div className="navbar-wrapper">
+      <div className="title-wrapper">
         <img src="https://cdn-assets.alltrails.com/assets/shared/AT_Main_Logo-da354e8947c2636e86fb27f60cbb037b.png" alt="Logo" />
         <div>at Lunch</div>
       </div>
-      <div className="options-container">
+      <div className="options-wrapper">
         <button>Filter</button>
         <input
           placeholder="Search for a restaurant"
